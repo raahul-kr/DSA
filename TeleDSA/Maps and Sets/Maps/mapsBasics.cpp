75e3a95bd1a2a8582492bf3a0886cb9dc036446b
raahul-kr <rahulkumar236@lpu.in>
2026-04-14 19:55:28 +0530

    added maps basics

---
+++ TeleDSA/Maps and Sets/Maps/mapsBasics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+    pair<string,char>p;
+    p.first = "Rahul";
+    p.second = 'A';
+    cout<<p.first<<" "<<p.second;
+
+    // unordered_map<string,int> m;
+
+}
