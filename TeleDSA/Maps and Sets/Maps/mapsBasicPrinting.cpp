af8ceca26bc45f6712bb0d09b8d534e1b22e972d
raahul-kr <rahulkumar236@lpu.in>
2026-04-15 23:34:21 +0530

    basics printing of maps

---
+++ TeleDSA/Maps and Sets/Maps/mapsBasicPrinting.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+    unordered_map<string,int>m;
+    pair<string,int>p1;
+    p1.first = "raghav";
+    p1.second = 76;
+    pair<string,int>p2;
+    p2.first = "harsh";
+    p2.second = 75;
+    pair<string,int>p3;
+    p3.first = "harshi";
+    p3.second = 753;
+
+
+    m.insert(p1);
+    m.insert(p2);
+    m.insert(p3);
+
+    for(pair<string,int>p:m){
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+}
