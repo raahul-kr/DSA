0b99f5e8f154d3fc83daf79ff10936cbf2a6eaa8
raahul-kr <rahulkumar236@lpu.in>
2026-04-16 10:16:19 +0530

    other printing method

--- TeleDSA/Maps and Sets/Maps/mapsBasicPrinting.cpp
+++ TeleDSA/Maps and Sets/Maps/mapsBasicPrinting.cpp
@@ -1,3 +1,31 @@
+// #include<iostream>
+// #include<unordered_map>
+// using namespace std;
+// int main(){
+//     unordered_map<string,int>m;
+//     pair<string,int>p1;
+//     p1.first = "raghav";
+//     p1.second = 76;
+//     pair<string,int>p2;
+//     p2.first = "harsh";
+//     p2.second = 75;
+//     pair<string,int>p3;
+//     p3.first = "harshi";
+//     p3.second = 753;
+
+
+//     m.insert(p1);
+//     m.insert(p2);
+//     m.insert(p3);
+
+//     for(pair<string,int>p:m){
+//         cout<<p.first<<" "<<p.second<<endl;
+//     }
+// }
+
+
+// ================= other some way
+
 #include<iostream>
 #include<unordered_map>
 using namespace std;
@@ -5,20 +33,23 @@ int main(){
     unordered_map<string,int>m;
     pair<string,int>p1;
     p1.first = "raghav";
-    p1.second = 76;
-    pair<string,int>p2;
-    p2.first = "harsh";
-    p2.second = 75;
-    pair<string,int>p3;
-    p3.first = "harshi";
-    p3.second = 753;
-
-
+    p1.second = 43;
     m.insert(p1);
-    m.insert(p2);
-    m.insert(p3);
 
-    for(pair<string,int>p:m){
+    m["Harsh"] = 15;
+    m["Lokesh"] = 67;
+
+    for(auto p:m){
         cout<<p.first<<" "<<p.second<<endl;
     }
+
+    cout<<m.size()<<endl;
+    m.erase("raghav");
+    m.erase("sanket");
+
+
+    for(auto p : m){
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+
 }
