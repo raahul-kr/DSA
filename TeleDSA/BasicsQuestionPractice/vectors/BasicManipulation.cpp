918d6ad541110b3e335bb3cc626ef05e67483340
raahul-kr <rahulkumar236@lpu.in>
2026-04-24 11:47:50 +0530

    added more manipulation

--- TeleDSA/BasicsQuestionPractice/vectors/BasicManipulation.cpp
+++ TeleDSA/BasicsQuestionPractice/vectors/BasicManipulation.cpp
@@ -77,10 +77,44 @@ void rightRotate(vector<int>arr){
     for(int i:arr){
         cout<<i<<" ";
     }
-
+    cout<<endl;
 }
+
+
+// move all the zeros to end(already void)
+void moveZeros(vector<int>&arr){
+    int j = 0;
+    for(int i = 0; i<arr.size(); i++){
+        if(arr[i] != 0){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+    cout<<"All the zeros to end: ";
+    for(int i: arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+
+// palindrone check
+bool isPalindrone(string &s,bool &result){
+    int l = 0, r = s.size()-1;
+    result = true;
+    while(l<r){
+        if(s[l] != s[r]){
+            result = false;
+            return result;
+        }
+        l++;
+        r--;
+    }
+    return result;
+}
+
 int main(){
-    vector<int>arr = {1,4,2,5,7,85,3,9};
+    vector<int>arr = {1,4,2,5,0,0,7,85,3,9};
 
     prinnt(arr);  // basics printing method
     sumOfArray(arr);  // simple sum of the array
@@ -89,5 +123,15 @@ int main(){
     swapAlternate(arr);
     leftRotate(arr);
     rightRotate(arr); 
+    moveZeros(arr);
+
+    // valid isPalindrone
+    string s = "madam";
+    bool res;
+    isPalindrone(s,res);
+    if(res){
+        cout<<"madam is palindrone;";
+    }
+    else cout<<"madam is not palindrone";
     return 0;
 }
